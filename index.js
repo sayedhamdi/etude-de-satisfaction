bd530fbcb6c62751101288553395dd1995755dea
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2020-03-12 15:19:37 +0100

    salahet fi barsha hajet

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ $(".hovreha3").hover(function(){
 $(".hovreha4").hover(function(){
     $(".p4").toggle();
 });
+
+function validateForm1() {
+    var x = document.forms["myForm"]["fname1"].value;
+    if (x == "") {
+        document.getElementById("demo1").innerHTML = "text";
+      return false;
+    }
+  }
+
+  function validateForm2() {
+    var x = document.forms["myForm"]["fname2"].value;
+    if (x == "") {
+        document.getElementById("demo2").innerHTML = "text";
+      return false;
+    }
+  }
+
+  $(button).click(function(){
+    validateForm1();
+    validateForm2();
+  });
