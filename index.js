b5cf676d280a1d6d351b6b598d45c31832d50be5
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2020-03-13 23:07:57 +0100

    tandhif

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-$(".p1").hide();
-$(".p2").hide();
-$(".p3").hide();
-$(".p4").hide();
-$(".hovreha1").hover(function(){
-    $(".p1").toggle();
-    $(".hovreha1").animate({
-        fontSize: "70px"
-    }, 1000);
-});
-$(".hovreha2").hover(function(){
-    $(".p2").toggle();
-});
-$(".hovreha3").hover(function(){
-    $(".p3").toggle();
-});
-$(".hovreha4").hover(function(){
-    $(".p4").toggle();
-});
-
 function validateForm1() {
     var x = document.forms["myForm"]["fname1"].value;
     if (x == "") {
@@ -38,3 +18,5 @@ function validateForm1() {
     validateForm1();
     validateForm2();
   });
+
+  
